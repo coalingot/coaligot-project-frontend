36086be0268b27da731efd8412d2391dea9cd7f2
LnRabbiT <blackneko1234@hotmail.com>
2022-02-07 23:57:52 +0700

    Can fetch item list

--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -1,12 +1,19 @@
 <template>
   <h1>Item List</h1>
 
-  <div class="item-list">
-    <ItemCard />
+  <div class="row">
+    <ItemCard :item="item" v-for="item in items" :key="item.id" />
+  </div>
+
+  <div class="footer">
+    <router-link to="#">
+      <button class="btn btn-add-color">Add item</button>
+    </router-link>
   </div>
 </template>
 
 <script>
+import ItemService from "../services/ItemService.js";
 import ItemCard from "../components/ItemCard.vue";
 
 export default {
@@ -14,8 +21,43 @@ export default {
   components: {
     ItemCard,
   },
+  data() {
+    return {
+      items: null,
+    };
+  },
+  created() {
+    ItemService.getAllItems()
+      .then((response) => {
+        this.items = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 };
 </script>
 
 <style>
+.footer {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+}
+.btn-add-color {
+  background-color: #2075f3;
+  color: white;
+  font-size: 16px;
+  padding: 15px;
+  border-radius: 25px;
+  border: 0;
+  cursor: pointer;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+  text-align: center;
+}
 </style>
